667e4e5a9600679a0a7a3ebaf2288b9da1d85074
wodpachua <molarag21996@gmail.com>
2024-05-02 14:24:31 +0300

    Add Project Configs

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    // clean: true,
+  },
+  mode: "development",
+  devtool: 'inline-source-map',
+  devServer: {
+    static: './dist',
+    compress: true,
+    port: 1789,
+    allowedHosts: 'auto',
+    open: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
+};
